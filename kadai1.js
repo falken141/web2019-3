88d246d678904c14f3621b7420831504453186e2
falken141 <zm18203@josai.ac.jp>
2020-02-12 08:00:54 +0000

    2/12

--- kadai1.js
+++ kadai1.js
@@ -1,6 +1,6 @@
 function kadai1(){
     document.getElementById('logo3').src='./image/super predetor.png';
-         setTimeout('kadai1a()',3000);
+     setTimeout('kadai1a()',3000);
 }
 
 function kadai1a(){
