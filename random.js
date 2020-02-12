49dda537559fc504b861abf95a91a74df4458c78
falken141 <zm18203@josai.ac.jp>
2020-02-12 16:51:03 +0000

    2/13

--- random.js
+++ random.js
@@ -14,3 +14,39 @@ function hantei1()
         window.location.href = 'soup5.html';
     }
 }
+
+function hantei2()
+{
+    if(document.getElementById('deme2').value<=70)
+    {
+        window.location.href = 'soup11.html'; 
+    }
+
+    else{
+        window.location.href = 'soup23.html';
+    }
+}
+
+function hantei3()
+{
+    if(document.getElementById('deme3').value<=70)
+    {
+        window.location.href = 'soup13.html'; 
+    }
+
+    else{
+        window.location.href = 'soup24.html';
+    }
+}
+
+function hantei4()
+{
+    if(document.getElementById('deme4').value<=45)
+    {
+        window.location.href = 'soup15.html'; 
+    }
+
+    else{
+        window.location.href = 'soup25.html';
+    }
+}
