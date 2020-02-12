bb50f97662e5646d0b1bdaea6672f62060582f34
falken141 <zm18203@josai.ac.jp>
2020-02-12 07:47:03 +0000

    2/12

---
+++ kadai1.js
@@ -0,0 +1,8 @@
+function kadai1(){
+    document.getElementById('logo3').src='./image/pledetar.png';
+         setTimeout('kadai1a()',3000);
+}
+
+function kadai1a(){
+    document.getElementById('logo3').src='./image/JUロゴ.png';
+}
